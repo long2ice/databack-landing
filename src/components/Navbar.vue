44742518d6b0a3593ade05e74ca0b4510a362c50
long2ice <long2ice@gmail.com>
2023-04-29 22:01:34 +0800

    feat: init

---
+++ src/components/Navbar.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="navbar bg-base-100">
+    <div class="navbar-start">
+      <a class="btn btn-ghost normal-case text-2xl">Databack</a>
+    </div>
+    <div class="navbar-center font-bold">
+      <ul class="menu menu-horizontal px-1">
+        <li><a href="https://docs.databack.dev" target="_blank">Documentation (writing)</a></li>
+        <li><a>Pricing</a></li>
+        <li><a href="mailto:long2ice@gmail.com">Contact</a></li>
+      </ul>
+    </div>
+    <div class="navbar-end">
+      <button class="btn-ghost btn-circle btn">
+        <label class="swap-rotate swap">
+          <input type="checkbox" data-toggle-theme="light,dark" />
+          <MdOutlinedWbSunny class="swap-off text-2xl" />
+          <CoMoon class="swap-on text-2xl" />
+        </label>
+      </button>
+      <a
+        href="https://github.com/long2ice/databack.git"
+        target="_blank"
+        class="btn btn-ghost text-2xl"
+      >
+        <AkGithubFill />
+      </a>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { themeChange } from 'theme-change'
+
+onMounted(() => {
+  themeChange(false)
+})
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <Navbar />
+  <div class="flex-1 flex justify-center items-center gap-20">
+    <div class="w-[40%] flex flex-col gap-10">
+      <div class="text-6xl font-bold text-blue-500">
+        Backup your data and restore it in one place
+      </div>
+      <div class="text-xl text-gray-400">
+        Support backup MySQL/PostgreSQL/Redis/MongoDB to any S3 compatible storages
+      </div>
+      <div class="flex gap-4">
+        <a
+          href="https://github.com/long2ice/databack.git"
+          target="_blank"
+          class="flex border p-3 items-center gap-2 rounded-md hover:bg-gray-100"
+        >
+          <AkGithubFill />
+          View on Github
+        </a>
+        <a
+          href="https://pro.databack.dev/login"
+          target="_blank"
+          class="flex border p-3 items-center gap-2 rounded-md hover:bg-gray-100"
+        >
+          Book a demo
+        </a>
+      </div>
+    </div>
+    <div class="w-[60%]">
+      <img src="/screenshot.png" class="rounded-md shadow-md" alt="home" />
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import Navbar from '@/components/Navbar.vue'
+</script>
+<style scoped></style>
